12185e1f08016f4e350dbe3cc8363085437dcebe
Ghazia Huma <ghaziahuma@gmail.com>
2026-02-07 12:27:44 +0500

    before adding listing files to cloudinary

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function CreateListing() {
+  
+  const[files, setFiles] = useState([]);
+  const handleImageSubmit=(e)=>{
+    if(files.length>0 && files.length<7){
+      const promises=[];
+      for(let i=0; i<files.length; i++){
+        promises.push(storeImage(files[i]));
+      }
+    }   
+  }
+
+  const storeImage = async (file) =>{
+    return new Promise((resolve, reject) =>{
+
+    })
+  }
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
@@ -98,8 +114,15 @@ export default function CreateListing() {
           </p>
 
           <div className='flex gap-4'>
-            <input className='p-3 border border-gray-300 rounded w-full' type='file' id='images' accept='image/*' multiple />
-            <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+
+            <input
+             onChange={(e)=>setFiles(e.target.files)}
+            className='p-3 border border-gray-300 rounded w-full' 
+            type='file' 
+            id='images' 
+            accept='image/*' multiple />
+
+            <button type='button' onClick={handleImageSubmit} className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
           </div>
           
           <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create Listing</button>
