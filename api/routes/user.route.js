964fbbc9224edf1fb5fda443835dc708cefcf71f
Ghazia Huma <ghaziahuma@gmail.com>
2026-02-16 14:15:39 +0500

    Add contact landlord functionality to the listing page.

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUser, getUserListings, test, updateUser } from '../controlllers/user.controller.js';
+import { deleteUser, getUserListings, getUserUser, test, updateUser } from '../controlllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router=express.Router();
 
@@ -7,4 +7,7 @@ router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUserUser)
+
 export default router;
+
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -5,6 +5,8 @@ import SwiperCore from 'swiper';
 import {Navigation} from 'swiper/modules';
 import 'swiper/css/bundle';
 import { FaBath, FaBed, FaChair, FaMapMarkerAlt, FaParking, FaShare } from 'react-icons/fa';
+import {useSelector} from 'react-redux';
+import Contact from '../components/Contact';
 
 
 export default function Listing() {
@@ -13,8 +15,10 @@ export default function Listing() {
     const[loading, setLoading]= useState(false);
     const[error, setError]= useState(false);
     const [copied, setCopied] = useState(false);
+    const [contact, setContact] = useState(false);
     const params = useParams();
- 
+    const {currentUser} = useSelector((state)=>state.user);
+
     useEffect(()=>{
         const fetchListing = async ()=>{
             try{
@@ -123,6 +127,12 @@ export default function Listing() {
                 </li>
                 
                </ul>
+
+              {currentUser && listing.userRef !== currentUser._id && !contact &&(
+                   <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>Contact landlord</button>
+              )}
+              {contact && <Contact listing={listing}/>}
+               
           </div>
            </div>
         )}
